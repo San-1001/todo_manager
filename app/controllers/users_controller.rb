b9859f2f45469266e44b2aaef7e9fc1a4b2be552
San-1001 <ukbkshs@gmail.com>
2020-05-18 22:30:05 +0530

    added ability to create new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,18 @@ class UsersController < ApplicationController
   def index
     render plain: User.order(:id).map { |user| user.to_pleasant_string }.join("\n")
   end
+
+  def create
+    name = params[:name]
+    email = params[:email]
+    password = params[:password]
+    new_user = User.create!(
+      name: name,
+      email: email,
+      password: password,
+
+    )
+    response_text = "Hey, your new user is created with the id #{new_user.id}"
+    render plain: response_text
+  end
 end
