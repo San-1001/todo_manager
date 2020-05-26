a0c74f89c35dc70a8010000610779aa9e0933162
San-1001 <ukbkshs@gmail.com>
2020-05-27 00:23:33 +0530

    overdue method changed again

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -5,7 +5,7 @@ class Todo < ActiveRecord::Base
   end
 
   def self.overdue
-    all.where("due_date < ? and (not completed)", Date.today).ordered
+    all.where("(due_date < ?) and (not completed)", Date.today).ordered
   end
   def self.due_today
     all.where("due_date = ?", Date.today)
